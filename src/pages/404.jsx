f14b1c1f49501780a45837cd53a21e61d3978295
elizadoltu <elizadoltuofficial@gmail.com>
2025-02-15 14:06:33 +0200

    Enhance 404 page layout with menu and cursor components

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Menu from "../components/Menu";
+import CursorContact from "../utils/CursorContact";
+import Cursor from "../utils/Cursor";
 
 export default function Page404() {
     return (
-        <div>
-            <h1 className="flex justify-center items-center text-3xl font-dirtyline">Hello</h1>
+        <div className="size-container-ideal leading-none justify-center items-center flex flex-col overflow-hidden">
+            <Menu />
+            <CursorContact />
+            <Cursor />
+            <div className="flex justify-center items-center font-dirtyline text-13xl mt-[20vh] leading-none">
+                <h1>4</h1>
+                <img src="/sticker-cat.png" alt="a sticker with a cute cat" className="w-2xl -mt-24"/>
+                <h1>4</h1>
+            </div>
+            <p className="font-switzer-regular uppercase leading-none -mt-[15vh]">Oops! Looks like you got lost in the catnip!</p>
         </div>
     );
 }

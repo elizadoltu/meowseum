aa0ae0f1ceb53001df4a8989617cc09f55e4f6a2
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2025-03-13 09:23:00 +0200

    Enhance 404 page layout for improved responsiveness and visual appeal

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -9,12 +9,20 @@ export default function Page404() {
             <Menu />
             <CursorContact />
             <Cursor />
-            <div className="flex justify-center items-center font-dirtyline text-13xl mt-[20vh] leading-none">
+            <div className="flex justify-center items-center font-dirtyline desktop:text-13xl tablet:text-10xl mobile:text-[3rem] mt-[20vh] leading-none gap-2">
                 <h1>4</h1>
-                <img src="/sticker-cat.png" alt="a sticker with a cute cat" className="w-2xl -mt-24"/>
+                <div className="flex justify-center items-center">
+                    <img 
+                        src="/sticker-cat.png" 
+                        alt="a sticker with a cute cat" 
+                        className="w-32 tablet:w-2xl -mt-12" 
+                    />
+                </div>
                 <h1>4</h1>
             </div>
-            <p className="font-general-regular uppercase leading-none -mt-[15vh]">Oops! Looks like you got lost in the catnip!</p>
+            <p className="font-general-regular uppercase leading-none desktop:-mt-[10vh] text-center px-4">
+                Oops! Looks like you got lost in the catnip!
+            </p>
         </div>
     );
 }
